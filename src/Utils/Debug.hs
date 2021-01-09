04434eae334787056b193e6253843313fcade882
Brekk <brekk@brekkbockrath.com>
2021-01-09 08:08:36 +0100

    simple tools

--- src/Utils/Debug.hs
+++ src/Utils/Debug.hs
@@ -8,27 +8,16 @@ import           System.Environment
 import           System.ReadEnvVar
 import           Text.Read                      ( readMaybe )
 
+-- |'xtrace' is a tagged log
 xtrace :: (Show a) => String -> a -> a
 xtrace s x = trace (s ++ " " <> ppShow x) x
 
--- instance Show a => Show String a where
---   xtrace s x = trace (s ++ " " <> ppShow x) x
-
+-- |'inspect' is 'xtrace' with a transform
 inspect :: (Show y) => String -> (x -> y) -> x -> x
 inspect s fn x = trace (s ++ " " <> ppShow (fn x)) x
 
+-- |'printWhen' is a conditional 'xtrace'
 printWhen :: Show p => (p -> Bool) -> String -> p -> p
 printWhen cond tag x | doPrint == True = xtrace tag x
                      | otherwise       = x
   where doPrint = cond x
-
--- printWhenEnv :: (Show x) => String -> x -> x
-
--- printWhenEnv envkey x = do
---   f <- lookupEnvDef envkey "NOT_SET"
---   return (xtrace envkey <$> f)
-  
-printWhenEnv envkey x = do
-  f <- lookupEnvDef envkey "NOT_SET"
-  return (xtrace envkey <$> f)
-  
